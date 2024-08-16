8fc7a4d64192470912546e0e4aa6fbbe96846d1b
MatheusTeixeira1999 <matheus2023teixeura@icloud.com>
2024-08-15 22:08:25 -0300

    index

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# tic-tac-toe
-tic-tac-toe created by me!
+
+# Tic-Tac-Toe in Next.js
+
+This is my Tic-Tac-Toe project built with Next.js.
+
+## How It Was Made
+
+To watch the video on how this project was created, visit my YouTube channel: [@000matheusteixeira](https://www.youtube.com/@000matheusteixeira).
+
+## How to Start the Project
+
+To run the project locally, follow these steps:
+
+1. Install the dependencies:
+   ```bash
+   npm install
+   
+2. Start the development server:
+   ```bash
+   npm run dev
---
+++ src/app/page.tsx
@@ -0,0 +1,120 @@
+'use client'
+import { useState } from "react";
+
+
+
+export default function Home() {
+
+  const [board, setBoard] = useState<Array<string | null>>(Array(9).fill(null))
+  const [ turn, setTurn] = useState<boolean>(true)
+  const [ player1, setPlayer1] = useState<number>(0)
+  const [ player2, setPlayer2] = useState<number>(0)
+  const [ draw, setDraw] = useState<number>(0)
+  
+
+  const handleClick = (index: number) => {
+    if (board[index] || checkWinner()) return; // prevent changing filled squares or playing after game end 
+    const newBoard = board.slice()
+    newBoard[index] = turn?'X':'O'
+    setBoard(newBoard)
+    
+    const winner = checkWinner(newBoard)
+    if (winner) {
+      switch(winner){
+        case 'X': 
+        setPlayer1(player1 + 1)
+        alert('Player1 win!!!')
+        setBoard(Array(9).fill(null))
+        setTurn(false)
+        break;
+        case 'O':
+          setPlayer2(player2 + 2)
+          alert('Player2 win!!!')
+          setBoard(Array(9).fill(null))
+          setTurn(true)
+          break;
+        default:
+          setDraw(draw + 1)
+          alert('Draw!!!')
+          setBoard(Array(9).fill(null))
+          setTurn(true)
+          break;
+      }
+    }
+    
+      
+
+
+
+    setTurn(!turn) // switch turn
+
+  }
+
+  const checkWinner = ( currentBoard = board) =>{
+    const winningCombinations =[
+      // horizont
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      
+      //vertical
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+
+      //diagonal
+      [0,4,8], // diag1
+      [2,4,6]  // diag2
+    ]
+
+    for(let combo of winningCombinations) {
+      const [a,b,c] = combo
+      if ( currentBoard[a] && currentBoard[a] === currentBoard[b] && currentBoard[a] === currentBoard[c]){
+        return currentBoard[a]
+      }
+    }
+    return currentBoard.includes(null)? null : 'Draw' ; // if no winner and board is full, it's a draw
+  }
+
+  const reset = ()=>{
+    setBoard(Array(9).fill(null))
+    setTurn(true)
+  }
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>TIC-TAC-TOE</h1>
+      <div className="grid grid-cols-3 gap-3">
+        <h1>
+          Player 1: {player1}
+        </h1>
+        <h1>
+          Draw: {draw}
+        </h1>
+
+      
+        <h1>
+        Player 2: {player2}
+        </h1>
+
+      </div>
+      <div className="grid grid-cols-3 gap-3">
+        {
+          board.map((value, index) => (
+            <button key={index} onClick={()=>{
+              handleClick(index)
+            }} className="border w-20 h-20 ">
+              {value}
+            </button>
+          ))
+        }
+      </div>
+      <button onClick={reset}>
+          Reset game
+        </button>
+
+
+    </main>
+  );
+}
